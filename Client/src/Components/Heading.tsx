3bda6bff3dc5a7d380876adcea6c2a5376f793ce
Afnankazi <afnanarifkazi2@gmail.com>
2025-08-04 20:06:01 +0530

    added typwriting effect

--- Client/src/Components/Heading.tsx
+++ Client/src/Components/Heading.tsx
@@ -4,37 +4,35 @@ import { TypewriterEffect } from "./ui/typewriter-effect";
 const Heading = () => {
   const words = [
     {
-      text: "Build",
+      text: "Turn",
     },
     {
-      text: "awesome",
+      text: "Your",
     },
     {
-      text: "apps",
+      text: "Thoughts",
     },
     {
-      text: "with",
+      text: "Into",
     },
     {
-      text: "Aceternity.",
+      text: "Secure,",
+    },
+    {
+      text: "Organized",
+    },
+    {
+      text: "Notes.",
       className: "text-blue-500 dark:text-blue-500",
     },
   ];
   return (
-    <div className="w-full fixed top-0 bg-neutral-900 z-50">
-      <div className="flex flex-col items-center justify-center h-[40rem] ">
-        <p className="text-neutral-600 dark:text-neutral-200 text-base  mb-10">
-          The road to freedom starts from here
+    <div className="w-full z-50">
+      <div className="flex flex-col items-center justify-center h-[30rem] ">
+        <p className=" dark:text-neutral-200 text-base  mb-10">
+          The #1 Secure note-taking app
         </p>
         <TypewriterEffect words={words} />
-        <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4 mt-10">
-          <button className="w-40 h-10 rounded-xl bg-black border dark:border-white border-transparent text-white text-sm">
-            Join now
-          </button>
-          <button className="w-40 h-10 rounded-xl bg-white text-black border border-black  text-sm">
-            Signup
-          </button>
-        </div>
       </div>
     </div>
   );
--- Client/src/pages/Landingpage.tsx
+++ Client/src/pages/Landingpage.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import Header from '../Components/Header'
+import Heading from '../Components/Heading'
 
 const Landingpage = () => {
   return (
     <div className="h-[200vh] bg-black">
-      <Header />
-      hi
+      <Heading/>
     </div>
   )
 }
